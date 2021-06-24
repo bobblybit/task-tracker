9c72a2a2f32ab54b70d9453df40cc0ce121ab6ab
bobblybit <juvenileandyou@gmail.com>
2021-06-24 22:02:02 +0100

    add toggle feature

--- src/App.js
+++ src/App.js
@@ -43,9 +43,27 @@ const deleteTask = async (id) => {
   setTasks (tasks.filter((task) => task.id !== id ))
 }
 
+
+const fetchtask = async (id) =>{
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+  return data
+} 
+
 //toggle rimder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? {...task,reminder: !task.reminder} : task))
+const toggleReminder = async (id) => {
+const taskToToggle = await fetchtask(id)
+const updTask = {... taskToToggle , reminder : !taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+    method:"PUT", 
+    headers:{"Content-type" : 'application/json'},
+    body: JSON.stringify(updTask)
+  })
+
+  const data =  await res.json() 
+
+  setTasks(tasks.map((task) => task.id === id ? {...task,reminder: data.reminder} : task))
 
 }
 
